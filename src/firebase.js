35d406b4eecfcde9d826a817596d4a501929acb1
Suyash <tiwarisuyash40@gmail.com>
2022-04-25 23:00:08 +0530

    Added checkout page and created the UI of Login page

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBk8sr86fRlWimCwez2MxHzk-h48mjVHWI",
+    authDomain: "greenkart-e0466.firebaseapp.com",
+    projectId: "greenkart-e0466",
+    storageBucket: "greenkart-e0466.appspot.com",
+    messagingSenderId: "349392427469",
+    appId: "1:349392427469:web:545af196c5ae5ebb008c84",
+    measurementId: "G-GYL673F7G7"
+  };
